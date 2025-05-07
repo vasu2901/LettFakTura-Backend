835d57b1552cd15aa89fec543410fa056f21c104
21Dipti <diptimahakalkar21@gmail.com>
2025-05-07 22:36:33 +0530

    Deploying code on vercel

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const start = async () => {
         await sequelize.authenticate();
         console.log('Database connected.');
         await sequelize.sync();
-        await fastify.listen({ port: 3000 });
+        await fastify.listen({ port: 3000, host: '0.0.0.0' });
         console.log('Server is running on port 3000');
     } catch (err) {
         fastify.log.error(err);
